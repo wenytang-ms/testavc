742c33504e804897a97d74a9edade6e6c3d4ef64
wenyutang-ms <wenyutang@microsoft.com>
2021-12-15 11:29:19 +0800

    chore: udpate

--- src/test.js
+++ src/test.js
@@ -3,3 +3,4 @@ const appKey = "1v~-zHq.2M0x6tW~JF.le~H6FcR3qLzo18";
 const paswt = "Admin123"
 const test = "123"
 const pawde = "nmcDQvkefIgnAbhKGO9zsad34LDxxCjiwDKwv2oPdAO8EsKqSmhdswfp3135vmwKzINVnNQJ8"
+const test1 = "diKbmefIufksgm9t5elLnIhRTi?@"
